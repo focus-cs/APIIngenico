b9e4394e3f22b9f0087d55b8d23c687bb9235d8c
lahoudie <lahoudie.eric@gmail.com>
2019-01-21 21:17:49 +0100

    Add files via upload

---
+++ 01_run_APIIngenico_FLOW_1.sh
@@ -0,0 +1,29 @@
+BATCH_PATH="/pathsample/APIIngenico"
+BATCH_MAIN="APIIngenico.jar"
+SCIFORMA_URL="https://ingenico-dev.sciforma.net/sciforma/"
+
+ROOT_DIR=$BATCH_PATH
+LIB_DIR=$ROOT_DIR/lib
+
+cd $LIB_DIR
+
+if test -f PSClient* ; then
+    rm -f PSClient*
+fi
+
+if test -f utilities* ; then
+    rm -f utilities*
+fi
+
+wget -O utilities.jar $SCIFORMA_URL/utilities.jar
+wget -O PSClient_en.jar $SCIFORMA_URL/PSClient_en.jar
+wget -O PSClient.jar $SCIFORMA_URL/PSClient.jar
+
+cd $ROOT_DIR
+
+JAVA_ARGS="-showversion"
+JAVA_ARGS="$JAVA_ARGS -Xms1024m"
+JAVA_ARGS="$JAVA_ARGS -Xmx8192m"
+JAVA_ARGS="$JAVA_ARGS -jar"
+
+java $JAVA_ARGS $BATCH_MAIN FLOW_1
---
+++ 02_run_APIIngenico_FLOW_2.bat
@@ -0,0 +1,34 @@
+set BATCH_PATH=C:/path/sample/APIIngenico
+set SCIFORMA_URL=https://ingenico-dev.sciforma.net/sciforma
+set BATCH_MAIN=APIIngenico.jar
+
+
+set ROOT_DIR=%BATCH_PATH%
+set LIB_DIR=%ROOT_DIR%/lib
+
+cd %LIB_DIR%
+
+IF EXIST "PSClient_en.jar" (
+   del "PSClient_en.jar"
+)
+IF EXIST "PSClient.jar" (
+   del "PSClient.jar"
+)
+IF EXIST "utilities.jar" (
+   del "utilities.jar"
+)
+
+wget.exe  -O utilities.jar %SCIFORMA_URL%/utilities.jar
+wget.exe  -O PSClient_en.jar %SCIFORMA_URL%/PSClient_en.jar
+wget.exe -O PSClient.jar %SCIFORMA_URL%/PSClient.jar
+
+cd %ROOT_DIR%
+
+set JAVA_ARGS=-showversion
+set JAVA_ARGS=%JAVA_ARGS% -Xms1024m
+set JAVA_ARGS=%JAVA_ARGS% -Xmx8192m
+set JAVA_ARGS=%JAVA_ARGS% -jar
+
+java %JAVA_ARGS% %BATCH_MAIN% FLOW_2
+pause
+ 
